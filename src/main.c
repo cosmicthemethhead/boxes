d6f0e6cb890fa3c23de42463cff751305c431842
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-07 02:14:18 +0200

    feat: added basic canvas drawing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+int main (void)
 {
-  printf("Hello, World!\n");
+  // holds canvas data
+  short d[2] = { 10, 10 }; // x, y
+
+  // basicly just a 2d for loop
+  short c_x = 0; short c_y = 0;
+  while (c_x < d[0]+1
+      && c_y < d[1])
+  {
+    printf("%i ", c_x);
+    c_x+=1;
+
+    // newline
+    if (c_x == d[0]) {
+      printf("\n");
+      c_x=0;
+      c_y+=1;
+    }
+  }
+
   return 0;
 }
