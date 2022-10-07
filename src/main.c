443e5022cf2487e4571b695a682d523e303e8840
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-07 03:40:56 +0200

    refactor: changed canvas draw loop index to vec2

--- src/main.c
+++ src/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include "util/vec2.h"
 
-int main (void)
-{
+int main (void) {
   // holds canvas data
   short d[2] = { 10, 10 }; // x, y
 
   // basicly just a 2d for loop
-  short c_x = 0; short c_y = 0;
-  while (c_x < d[0]+1
-      && c_y < d[1])
+  vec2i i = {0, 0};
+  while (i.x < d[0]+1
+      && i.y < d[1])
   {
-    printf("%i ", c_x);
-    c_x+=1;
+    printf("⋅ ");
+    i.x+=1;
 
     // newline
-    if (c_x == d[0]) {
+    if (i.x == d[0]) {
       printf("\n");
-      c_x=0;
-      c_y+=1;
+      i.x=0;
+      i.y+=1;
     }
   }
 
