4df37d67fff8ef915292f89a9bc5997076eb4052
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-08 02:37:47 +0200

    refactor: removed `typedef`

--- src/util/entity.h
+++ src/util/entity.h
@@ -3,11 +3,10 @@
 
 #include "util/vec2.h"
 
-// HACK: reserve 4 bytes for chars that could use 1
-typedef struct {
+struct entity {
   // reserve 4 bytes for each icon
   char icon[4];
   vec2i pos;
-} entity;
+};
 
 #endif
